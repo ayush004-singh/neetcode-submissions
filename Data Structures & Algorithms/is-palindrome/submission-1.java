7aa2f58d94f99050e242aa73cf4d4908f632fa8e
Ayush Singh <aayushkashyapa415@gmail.com>
2026-06-08 19:12:37 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    private static boolean check(String s,int left,int right){
+        if(left>=right) return true;
+     if(s.charAt(left)!=s.charAt(right)){
+            return false;
+        }
+
+        return check(s,left+1,right-1);
+    }
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");  
+        return check(s,0,s.length()-1);
+    }
+}
