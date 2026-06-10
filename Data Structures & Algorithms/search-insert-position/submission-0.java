eaa17a4f59f0b6aca14b918018532d54fe0c6f7b
Ayush Singh <aayushkashyapa415@gmail.com>
2026-06-10 22:56:27 +0530

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        int ans=0;
+        while(low<=high){
+            int mid = low+(high-low);
+            if(nums[mid]<target){
+                ans=Math.max(ans,mid+1);
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+
+        return ans;
+    }
+}
